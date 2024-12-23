28febcd7354bc9a4bc1d1a8697ad5f52acafaf96
Alex Zaitsev <alex.zaitsev.pro@gmail.com>
2024-12-22 21:16:21 -0700

    removed explicit job cancellation as viewModelScope will be automatically discarded when VM is cleared

--- app/src/main/java/com/alexzaitsev/fetchtest/ui/screen/main/MainViewModel.kt
+++ app/src/main/java/com/alexzaitsev/fetchtest/ui/screen/main/MainViewModel.kt
@@ -4,7 +4,6 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.alexzaitsev.fetchtest.data.repository.ItemsRepository
 import kotlinx.collections.immutable.toImmutableList
-import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
@@ -14,21 +13,14 @@ class MainViewModel(private val itemsRepository: ItemsRepository) : ViewModel() 
     private val _state = MutableStateFlow(MainState.Initial)
     val state: StateFlow<MainState> = _state
 
-    private var loadingJob: Job? = null
     private val isDataLoaded: Boolean
         get() = state.value.items.isNotEmpty() && state.value.error == null
-
-    override fun onCleared() {
-        super.onCleared()
-        loadingJob?.cancel()
-    }
 
     fun loadData() {
         if (isDataLoaded) {
             return
         }
-        loadingJob?.cancel()
-        loadingJob = viewModelScope.launch {
+        viewModelScope.launch {
             _state.value = MainState.Initial
 
             itemsRepository.getItems().fold({ items ->
